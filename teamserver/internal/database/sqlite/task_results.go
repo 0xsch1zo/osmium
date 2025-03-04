45df24aa6b5aa76306671c6098f2275c5eb0416e
sentientbottleofwine <sentientbottleofwine@gmail.com>
2025-03-04 22:09:35 +0100

    Cleanup clutter

--- teamserver/internal/database/sqlite/task_results.go
+++ teamserver/internal/database/sqlite/task_results.go
@@ -7,29 +7,6 @@ import (
 	"github.com/sentientbottleofwine/osmium/teamserver/service"
 )
 
-/*func (trr *TaskResultsRepository) SaveTaskResults(agentId uint64, taskResults []teamserver.TaskResultIn) error {
-	var queryBuilder strings.Builder
-	queryBuilder.WriteString("INSERT INTO TaskResults (AgentId, TaskId, Output) VALUES")
-	values := []interface{}{}
-
-	for _, taskResults := range taskResults {
-		queryBuilder.WriteString("(?, ?, ?),")
-		values = append(values, agentId, taskResults.TaskId, taskResults.Output)
-	}
-
-	query := queryBuilder.String()
-	query = strings.TrimSuffix(query, ",")
-
-	stmt, err := trr.databaseHandle.Prepare(query)
-	if err != nil {
-		return err
-	}
-
-	_, err = stmt.Exec(values...)
-	return err
-
-}*/
-
 func (trr *TaskResultsRepository) SaveTaskResult(agentId uint64, taskResult *teamserver.TaskResultIn) error {
 	query := "INSERT INTO TaskResults (AgentId, TaskId, Output) VALUES(?, ?, ?)"
 
@@ -55,24 +32,3 @@ func (trr *TaskResultsRepository) GetTaskResult(agentId uint64, taskId uint64) (
 
 	return &taskResult, nil
 }
-
-/*func (trr *TaskResultsRepository) GetTaskResults(agentId uint64) ([]teamserver.TaskResultOut, error) {
-	query := "SELECT TaskResults.TaskId, Task, Output FROM TaskResults INNER JOIN TaskQueue ON TaskResults.TaskId = TaskQueue.TaskId WHERE agentId = ?"
-	taskResultsSqlRows, err := trr.databaseHandle.Query(query, agentId)
-	if err != nil {
-		return nil, err
-	}
-
-	taskResults := []teamserver.TaskResultOut{}
-	for taskResultsSqlRows.Next() {
-		taskResult := teamserver.TaskResultOut{}
-		err := taskResultsSqlRows.Scan(&taskResult.TaskId, &taskResult.Task, &taskResult.Output)
-		if err != nil {
-			return nil, err
-		}
-
-		taskResults = append(taskResults, taskResult)
-	}
-
-	return taskResults, nil
-}*/
