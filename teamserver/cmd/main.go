14b8a98354e76a985f6b83e84a05bcee1f5d9898
sentientbottleofwine <sentientbottleofwine@gmail.com>
2025-03-08 18:38:49 +0100

    add configuration support

--- teamserver/cmd/main.go
+++ teamserver/cmd/main.go
@@ -1,54 +1,32 @@
 package main
 
 import (
-	"errors"
 	"flag"
 	"log"
 
+	"github.com/sentientbottleofwine/osmium/teamserver/internal/config"
 	"github.com/sentientbottleofwine/osmium/teamserver/internal/database"
 	"github.com/sentientbottleofwine/osmium/teamserver/internal/http"
 )
 
-func checkIfArgDefined(argName string) (bool, error) {
-	if !flag.Parsed() {
-		return false, errors.New("flag.Parse() has not been yet called")
-	}
-
-	var found bool
-	flag.Visit(func(flag *flag.Flag) {
-		if flag.Name == argName {
-			found = true
-		}
-	})
-
-	return found, nil
-}
-
 func main() {
-	port := flag.Int("port", 8080, "Specify the port that the server will run on")
-	// Small hack to make the default look sane
-	portFlag := flag.Lookup("port")
-	portFlag.DefValue = "8080/8443"
-
-	https := flag.Bool("https", false, "If https is enabled then both cert and key are required")
-	certificate := flag.String("cert", "", "Specify the https certificate")
-	key := flag.String("key", "", "Specify encryption key")
-
+	configPath := flag.String("config", "", "Explicitly specify conifg location")
 	flag.Parse()
 
-	if *https {
-		if len(*certificate) == 0 || len(*key) == 0 {
-			log.Fatal("Key or certificate was not supplied")
-		}
+	var serverConfig *config.Config
+	var err error
+	if len(*configPath) != 0 {
+		serverConfig, err = config.ParseConfig(*configPath)
+	} else {
+		serverConfig, err = config.ParseDefaultConfig()
+	}
+	if err != nil {
+		log.Fatal(err)
+	}
 
-		portExplicitlyDefined, err := checkIfArgDefined("port")
-		if err != nil {
-			log.Fatal(err)
-		}
-
-		if !portExplicitlyDefined {
-			*port = 8443
-		}
+	err = config.ValidateConfig(serverConfig)
+	if err != nil {
+		log.Fatal(err)
 	}
 
 	db, err := database.NewDatabase("teamserver.db")
@@ -56,9 +34,9 @@ func main() {
 		log.Fatal(err)
 	}
 
-	server := http.NewServer(*port, db)
-	if *https {
-		log.Fatal(server.ListenAndServeTLS(*certificate, *key))
+	server := http.NewServer(int(serverConfig.Port), db)
+	if serverConfig.Https {
+		log.Fatal(server.ListenAndServeTLS(serverConfig.CertificatePath, serverConfig.KeyPath))
 	} else {
 		log.Fatal(server.ListenAndServe())
 	}
---
+++ teamserver/internal/config/config.go
@@ -0,0 +1,57 @@
+package config
+
+import (
+	"errors"
+	"os"
+
+	"github.com/BurntSushi/toml"
+)
+
+const defaultConfigPath = "config.toml"
+
+type Config struct {
+	Port            uint16 `toml:"port"`
+	Https           bool   `toml:"https"`
+	CertificatePath string `toml:"cert"`
+	KeyPath         string `toml:"key"`
+	Username        string `toml:"username"`
+	Password        string `toml:"password"`
+}
+
+func ParseConfig(path string) (*Config, error) {
+	configFile, err := os.Open(path)
+	if err != nil {
+		return nil, err
+	}
+	defer configFile.Close()
+
+	var config Config
+	_, err = toml.NewDecoder(configFile).Decode(&config)
+	return &config, err
+}
+
+func ParseDefaultConfig() (*Config, error) {
+	_, err := os.Stat(defaultConfigPath)
+	if errors.Is(err, os.ErrNotExist) {
+		return &Config{
+			Port:     8080,
+			Https:    false,
+			Username: "osmium",
+			Password: "I'm losing sanity",
+		}, nil
+	} else if err != nil {
+		return nil, err
+	}
+
+	return ParseConfig(defaultConfigPath)
+}
+
+func ValidateConfig(config *Config) error {
+	if config.Https {
+		if len(config.CertificatePath) == 0 || len(config.KeyPath) == 0 {
+			return errors.New("Certificate or key path not specified despite https mode enabled")
+		}
+	}
+
+	return nil
+}
