da4387f66f978d0d3e5feea443d19bfde8b11657
sentientbottleofwine <sentientbottleofwine@gmail.com>
2025-03-28 19:20:53 +0100

    Fix bug in GetTaskResult resulting in wrong output being returned

--- teamserver/internal/database/sqlite/task_results.go
+++ teamserver/internal/database/sqlite/task_results.go
@@ -2,6 +2,8 @@ package sqlite
 
 import (
 	"database/sql"
+	"log"
+
 	"github.com/sentientbottleofwine/osmium/teamserver"
 )
 
@@ -17,10 +19,11 @@ func (trr *TaskResultsRepository) SaveTaskResult(agentId uint64, taskResult *tea
 }
 
 func (trr *TaskResultsRepository) GetTaskResult(agentId uint64, taskId uint64) (*teamserver.TaskResultOut, error) {
-	query := "SELECT Task, Output FROM TaskResults INNER JOIN Tasks ON Tasks.TaskId = ? WHERE TaskResults.AgentId = ?"
-	taskResultsSqlRow := trr.databaseHandle.QueryRow(query, taskId, agentId)
+	query := "SELECT Task, Output FROM TaskResults INNER JOIN Tasks ON Tasks.TaskId = TaskResults.TaskId WHERE TaskResults.AgentId = ? AND TaskResults.TaskId = ?"
+	taskResultsSqlRow := trr.databaseHandle.QueryRow(query, agentId, taskId)
 	taskResult := teamserver.TaskResultOut{}
 	err := taskResultsSqlRow.Scan(&taskResult.Task, &taskResult.Output)
+	log.Print()
 	if err != nil {
 		return nil, err
 	}
