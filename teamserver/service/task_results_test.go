e52cc274ef53856968b65e02cef673c62354c3cb
sentientbottleofwine <sentientbottleofwine@gmail.com>
2025-04-17 21:41:56 +0200

    Add test for GetTaskResults

--- teamserver/service/task_results_test.go
+++ teamserver/service/task_results_test.go
@@ -17,7 +17,7 @@ func isClientError(err error) bool {
 	return errors.As(err, &target)
 }
 
-func TestSaveAndGetTaskResults(t *testing.T) {
+func TestSaveAndGetTaskResult(t *testing.T) {
 	testedServices, err := newTestedServices()
 	if err != nil {
 		t.Fatal(err)
@@ -83,6 +83,58 @@ func TestSaveAndGetTaskResults(t *testing.T) {
 			t.Fatal("No error detected with incorrect arguments")
 		} else if !isClientError(err) {
 			t.Fatal("Non client error returned with incorrect args")
+		}
+	}
+}
+
+func TestGetTaskResults(t *testing.T) {
+	testedServices, err := newTestedServices()
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	validAgent, err := testedServices.agentService.AddAgent()
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	const testTask = "test task"
+	validTaskIds := make([]uint64, 2)
+	validTaskIds[0], err = testedServices.tasksService.AddTask(validAgent.AgentId, testTask)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	validTaskIds[1], err = testedServices.tasksService.AddTask(validAgent.AgentId, testTask)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	given := []saveTaskResutlsTestCase{
+		// Success
+		{validAgent.AgentId, teamserver.TaskResultIn{TaskId: validTaskIds[0], Output: "some output"}},
+		{validAgent.AgentId, teamserver.TaskResultIn{TaskId: validTaskIds[1], Output: "some output"}},
+	}
+
+	for testCaseIndex := 0; testCaseIndex < 2; testCaseIndex++ {
+		err = testedServices.taskResultsService.SaveTaskResult(given[testCaseIndex].agent, &given[testCaseIndex].taskResultsIn)
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		taskResults, err := testedServices.taskResultsService.GetTaskResults(validAgent.AgentId)
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		if taskResults[testCaseIndex].Output != given[testCaseIndex].taskResultsIn.Output ||
+			taskResults[testCaseIndex].TaskId != validTaskIds[testCaseIndex] ||
+			taskResults[testCaseIndex].Task != testTask {
+			fatalErrUnexpectedData(t, "Task result data doesn't match", struct {
+				output string
+				taskId uint64
+				task   string
+			}{given[testCaseIndex].taskResultsIn.Output, validTaskIds[testCaseIndex], testTask}, taskResults[testCaseIndex])
 		}
 	}
 }
