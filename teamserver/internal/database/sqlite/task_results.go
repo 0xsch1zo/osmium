a12931c53492622e6773114353a1ebe7e80e51b4
sentientbottleofwine <sentientbottleofwine@gmail.com>
2025-05-04 10:50:20 +0200

    Fix GetTaskResult and GetTaskResults bug. Add tests for it.

--- teamserver/internal/database/sqlite/task_results.go
+++ teamserver/internal/database/sqlite/task_results.go
@@ -18,7 +18,7 @@ func (trr *TaskResultsRepository) SaveTaskResult(agentId uint64, taskResult *tea
 }
 
 func (trr *TaskResultsRepository) GetTaskResult(agentId uint64, taskId uint64) (*teamserver.TaskResultOut, error) {
-	query := "SELECT Task, Output FROM TaskResults INNER JOIN Tasks ON Tasks.TaskId = TaskResults.TaskId WHERE TaskResults.AgentId = ? AND TaskResults.TaskId = ?"
+	query := "SELECT Task, Output FROM TaskResults INNER JOIN Tasks ON Tasks.TaskId = TaskResults.TaskId AND Tasks.AgentId = TaskResults.AgentId WHERE TaskResults.AgentId = ? AND TaskResults.TaskId = ?"
 	taskResultsSqlRow := trr.databaseHandle.QueryRow(query, agentId, taskId)
 	taskResult := teamserver.TaskResultOut{}
 	err := taskResultsSqlRow.Scan(&taskResult.Task, &taskResult.Output)
@@ -32,7 +32,7 @@ func (trr *TaskResultsRepository) GetTaskResult(agentId uint64, taskId uint64) (
 }
 
 func (trr *TaskResultsRepository) GetTaskResults(agentId uint64) ([]teamserver.TaskResultOut, error) {
-	query := "SELECT Task, Output, TaskResults.TaskId FROM TaskResults INNER JOIN Tasks ON Tasks.TaskId = TaskResults.TaskId WHERE TaskResults.AgentId = ?"
+	query := "SELECT Task, Output, TaskResults.TaskId FROM TaskResults INNER JOIN Tasks ON Tasks.TaskId = TaskResults.TaskId AND Tasks.AgentId = TaskResults.AgentId WHERE TaskResults.AgentId = ?"
 	taskResultsSqlRows, err := trr.databaseHandle.Query(query, agentId)
 	if err != nil {
 		return nil, err
--- teamserver/service/task_results_test.go
+++ teamserver/service/task_results_test.go
@@ -2,6 +2,7 @@ package service_test
 
 import (
 	"errors"
+	"strconv"
 	"testing"
 
 	"github.com/sentientbottleofwine/osmium/teamserver"
@@ -23,31 +24,32 @@ func TestSaveAndGetTaskResult(t *testing.T) {
 		t.Fatal(err)
 	}
 
-	validAgent, err := testedServices.agentService.AddAgent()
-	if err != nil {
-		t.Fatal(err)
+	validAgents := make([]teamserver.Agent, 2)
+	for i := range len(validAgents) {
+		agent, err := testedServices.agentService.AddAgent()
+		if err != nil {
+			t.Fatal(err)
+		}
+		validAgents[i] = *agent
 	}
 
 	const testTask = "test task"
 	validTaskIds := make([]uint64, 2)
-	validTaskIds[0], err = testedServices.tasksService.AddTask(validAgent.AgentId, testTask)
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	validTaskIds[1], err = testedServices.tasksService.AddTask(validAgent.AgentId, testTask)
-	if err != nil {
-		t.Fatal(err)
+	for i := range len(validTaskIds) {
+		validTaskIds[i], err = testedServices.tasksService.AddTask(validAgents[0].AgentId, testTask)
+		if err != nil {
+			t.Fatal(err)
+		}
 	}
 
 	given := []saveTaskResutlsTestCase{
 		// Success
-		{validAgent.AgentId, teamserver.TaskResultIn{TaskId: validTaskIds[0], Output: "some output"}},
-		{validAgent.AgentId, teamserver.TaskResultIn{TaskId: validTaskIds[1], Output: "some output"}},
+		{validAgents[0].AgentId, teamserver.TaskResultIn{TaskId: validTaskIds[0], Output: "some output"}},
+		{validAgents[0].AgentId, teamserver.TaskResultIn{TaskId: validTaskIds[1], Output: "some output"}},
 
 		// Failure
 		{2137, teamserver.TaskResultIn{TaskId: validTaskIds[0], Output: "some output"}},
-		{validAgent.AgentId, teamserver.TaskResultIn{TaskId: 2137, Output: "some output"}},
+		{validAgents[0].AgentId, teamserver.TaskResultIn{TaskId: 2137, Output: "some output"}},
 		{2137, teamserver.TaskResultIn{TaskId: 2137, Output: "some output"}},
 	}
 
@@ -59,7 +61,7 @@ func TestSaveAndGetTaskResult(t *testing.T) {
 			t.Fatal(err)
 		}
 
-		taskResult, err := testedServices.taskResultsService.GetTaskResult(validAgent.AgentId, given[testCaseIndex].taskResultsIn.TaskId)
+		taskResult, err := testedServices.taskResultsService.GetTaskResult(validAgents[0].AgentId, given[testCaseIndex].taskResultsIn.TaskId)
 		if err != nil {
 			t.Fatal(err)
 		}
@@ -68,10 +70,10 @@ func TestSaveAndGetTaskResult(t *testing.T) {
 			taskResult.TaskId != validTaskIds[testCaseIndex] ||
 			taskResult.Task != testTask {
 			fatalErrUnexpectedData(t, "Task result data doesn't match", struct {
-				output string
 				taskId uint64
 				task   string
-			}{given[testCaseIndex].taskResultsIn.Output, validTaskIds[testCaseIndex], testTask}, taskResult)
+				output string
+			}{validTaskIds[testCaseIndex], testTask, given[testCaseIndex].taskResultsIn.Output}, taskResult)
 		}
 
 		testCaseIndex++
@@ -83,6 +85,85 @@ func TestSaveAndGetTaskResult(t *testing.T) {
 			t.Fatal("No error detected with incorrect arguments")
 		} else if !isClientError(err) {
 			t.Fatal("Non client error returned with incorrect args")
+		}
+	}
+}
+
+func TestGetTaskResultAndResultsMultiAgent(t *testing.T) {
+	testedServices, err := newTestedServices()
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	agents := make([]teamserver.Agent, 2)
+	tasks := make([]teamserver.Task, len(agents))
+	const testTaskPref = "test task"
+	for i := range len(agents) {
+		agent, err := testedServices.agentService.AddAgent()
+		if err != nil {
+			t.Fatal(err)
+		}
+		agents[i] = *agent
+
+		tasks[i].Task = testTaskPref + strconv.FormatUint(agent.AgentId, 10)
+		tasks[i].TaskId, err = testedServices.tasksService.AddTask(agent.AgentId, tasks[i].Task)
+		if err != nil {
+			t.Fatal(err)
+		}
+	}
+
+	taskResults := make([]teamserver.TaskResultIn, len(tasks))
+	const testTaskResultPref = "test output"
+	for i := range len(taskResults) {
+		taskResults[i] = teamserver.TaskResultIn{
+			TaskId: tasks[i].TaskId,
+			Output: testTaskResultPref + strconv.FormatUint(agents[i].AgentId, 10),
+		}
+
+		err = testedServices.taskResultsService.SaveTaskResult(agents[i].AgentId, &taskResults[i])
+		if err != nil {
+			t.Fatal(err)
+		}
+	}
+
+	for i := range len(agents) {
+		taskResultGot, err := testedServices.taskResultsService.GetTaskResult(agents[i].AgentId, tasks[i].TaskId)
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		if taskResultGot.Output != taskResults[i].Output ||
+			taskResultGot.TaskId != taskResults[i].TaskId ||
+			taskResultGot.Task != tasks[i].Task {
+			fatalErrUnexpectedData(t, "Task result data doesn't match", struct {
+				taskId uint64
+				task   string
+				output string
+			}{tasks[i].TaskId, tasks[i].Task, taskResults[i].Output}, *taskResultGot)
+		}
+
+		taskResultsGot, err := testedServices.taskResultsService.GetTaskResults(agents[i].AgentId)
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		if len(taskResultsGot) != 1 {
+			t.Error("Unexpected task results recieved length")
+			fatalErrUnexpectedData(t, "Task result data doesn't match", struct {
+				taskId uint64
+				task   string
+				output string
+			}{tasks[i].TaskId, tasks[i].Task, taskResults[i].Output}, taskResultsGot)
+		}
+
+		if taskResultsGot[0].Output != taskResults[i].Output ||
+			taskResultsGot[0].TaskId != taskResults[i].TaskId ||
+			taskResultsGot[0].Task != tasks[i].Task {
+			fatalErrUnexpectedData(t, "Task result data doesn't match", struct {
+				taskId uint64
+				task   string
+				output string
+			}{tasks[i].TaskId, tasks[i].Task, taskResults[i].Output}, *taskResultGot)
 		}
 	}
 }
