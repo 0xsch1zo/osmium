b27ddd34a9964bf58c8ed425bd7ebe49416438de
sentientbottleofwine <sentientbottleofwine@gmail.com>
2025-05-15 17:20:21 +0200

    Handle auth refresh error better

--- teamserver/static/js/auth_refresh.js
+++ teamserver/static/js/auth_refresh.js
@@ -1,5 +1,9 @@
 async function authRefresh() {
     const resp = await fetch("/api/auth/refreshTime")
+    if (resp.status == 401) {
+        window.location.reload()
+        return
+    }
     const json = await resp.json()
     const refTime = Date.parse(json.RefTime)
     const waitDuration = refTime - Date.now()
