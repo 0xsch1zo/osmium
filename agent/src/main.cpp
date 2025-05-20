16945a9ac3ebbe964125facf2671fbcdd8cc61c5
sentientbottleofwine <sentientbottleofwine@gmail.com>
2025-05-20 19:32:00 +0200

    Remove accidental testing code from agent

--- agent/src/main.cpp
+++ agent/src/main.cpp
@@ -1,9 +1,6 @@
 #include "agent.hpp"
-#include "utils.hpp"
-#include <iostream>
 
 int main(int argc, char *argv[]) {
-  std::cout << exec("echo %USERNAME%");
   std::unique_ptr<Agent> agent;
   if (argc == 2) {
     agent = std::make_unique<Agent>(std::stoi(argv[1]), "");
